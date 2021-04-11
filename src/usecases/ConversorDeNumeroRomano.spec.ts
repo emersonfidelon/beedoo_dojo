c341310b27ddc4587880f0715f7bd074f1d14c80
Paulo Tiago <paulotiagocm@hotmail.com>
2021-04-11 11:24:32 -0300

    test: outro algarismo romano e abstração da logica

--- src/usecases/ConversorDeNumeroRomano.spec.ts
+++ src/usecases/ConversorDeNumeroRomano.spec.ts
@@ -2,9 +2,15 @@ import { ConversorDeNumeroRomano } from './ConversorDeNumeroRomano'
 
 describe('ConversorDeNumeroRomano Testes', () => {
   test('Deve retornar 1 para algarismo romano I', () => {
-    
-    const romano = new ConversorDeNumeroRomano().converter("I");
+    const algarismoRomano:String = "I";
+    const romano = new ConversorDeNumeroRomano().converter(algarismoRomano);
 
     expect(romano).toBe(1);
+  });
+  test('Deve retornar 5 para algarismo romano V', () => {
+    const algarismoRomano:String = "V";
+    const romano = new ConversorDeNumeroRomano().converter(algarismoRomano);
+
+    expect(romano).toBe(5);
   });
 });
--- src/usecases/ConversorDeNumeroRomano.ts
+++ src/usecases/ConversorDeNumeroRomano.ts
@@ -1,6 +1,21 @@
 class ConversorDeNumeroRomano {
-    converter(algorismoRomano: String): number {
-        return 1;
+    converter(algarismoRomano: String): number {
+        return new TabelaRomana().getTabela(algarismoRomano);
+    }
+}
+
+interface ITabela {
+    readonly tabela;
+}
+
+class TabelaRomana implements ITabela {
+    tabela = {
+        "I": 1,
+        "V": 5,
+    }
+
+    public getTabela(algarismo) : number {
+        return this.tabela[algarismo];
     }
 }
 
