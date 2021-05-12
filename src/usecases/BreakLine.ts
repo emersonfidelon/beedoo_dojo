ff0d84b128c16b3f61ba95d2e992a14f9e022175
Emerson Santana <emersonfidelon@gmail.com>
2021-05-12 18:11:19 -0300

    Send code to repository

--- src/usecases/BreakLine.ts
+++ src/usecases/BreakLine.ts
@@ -1,5 +1,43 @@
 class BreakLine {
-    
+  frase: string;
+  colunas: number;
+  countQuebraLinha: number = 0;
+
+  constructor(frase:string, colunas:number){
+    this.frase = frase;
+    this.colunas = colunas;
+
+      this.validate();
+  }
+
+handle(){
+      let fraseFinal = '';
+      for(const frase of this.frase.split('')){
+        if(fraseFinal.length < this.colunas){
+              fraseFinal.concat(` ${frase}`)
+ 
+           } else {
+                fraseFinal.concat('\n')
+                this.countQuebraLinha++;
+            }   
+        }
+
+        return fraseFinal;
+    }
+
+    validate() {
+        if (this.frase.length === 0) {
+            throw new Error('A frase deve conter ao menos um caractere')
+        }
+
+        if (this.colunas < 1) {
+            throw new Error('A quantidade de coluna deve ser maior do que zero')
+        }
+
+        if (!Number.isInteger(this.colunas)) {
+            throw new Error('A quantidade de coluna deve ser um inteiro')
+        }
+    }
 }
 
 export { BreakLine }
