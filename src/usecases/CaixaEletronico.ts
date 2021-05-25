9a3b8ab7aeb462a82b3d9e6fbf74b6ad056413d0
Leticiaf.dev <leticiaf.dev@gmail.com>
2021-05-24 23:33:19 -0300

    Adicionando limite de notas

--- src/usecases/CaixaEletronico.ts
+++ src/usecases/CaixaEletronico.ts
@@ -1,6 +1,11 @@
 export function caixaEletronico(valor_saque:number) {
 
-    const notas_disponiveis = [100, 50, 20,10];
+    let notas_disponiveis = [];
+    notas_disponiveis.push({valor:100, quantidade: 2});
+    notas_disponiveis.push({valor:50, quantidade: 10});
+    notas_disponiveis.push({valor:20, quantidade: 10});
+    notas_disponiveis.push({valor:10, quantidade: 10});
+
     let valor_restante = valor_saque,
         notas = [], infos_to_return = 'Entregar ';
 
@@ -8,30 +13,30 @@ export function caixaEletronico(valor_saque:number) {
         return 'Não é possível sacar um valor negativo.';
     }
 
-    if(notas_disponiveis.includes(valor_saque)) {
-        return `Entregar 1 nota de R$${valor_saque},00.`;
-    }
-    
     if(valor_saque % 10 !==0){
         return 'Ná há notas disponíveis para o valor informado.';
     }
 
+    if(notas_disponiveis.find(nota => nota.valor == valor_saque && nota.quantidade > 0)) {
+        return `Entregar 1 nota de R$${valor_saque},00.`;
+    }   
 
     notas_disponiveis.map(nota => {
-        let quantidade = valor_restante / nota | 0 ;
-        valor_restante -= quantidade * nota;
+        let quantidade = Math.min(valor_restante / nota.valor | 0, nota.quantidade) ;
+        valor_restante -= quantidade * nota.valor;
         
         notas.push(JSON.stringify({ valor: nota, quantidade: quantidade }));   
         
+        //Prepara o texto de resposta
         quantidade > 0 ?
         infos_to_return += 
-        `${quantidade} ${quantidade === 1 ? 'nota' : 'notas'} de R$${nota},00, ` : 0;
-        
+        `${quantidade} ${quantidade === 1 ? 'nota' : 'notas'} de R$${nota.valor},00, ` : 0;        
     })
     
     return manipula_texto(infos_to_return)+'.'
 }
 
+//Remove as virgulas desnecessárias
 function manipula_texto(text) {
     if(  text.indexOf( ", " ) > 0 && text.indexOf( ", " ) != text.lastIndexOf( ", " )  ) { 
         text = text.slice( 0, text.lastIndexOf( ", " ) ); 
