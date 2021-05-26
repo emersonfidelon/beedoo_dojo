1b927f13755a2dc1cb8664ff3284ac96b6d1a124
Emerson Santana <emersonfidelon@gmail.com>
2021-05-26 15:42:07 -0300

    pluss FizzBuzz

--- src/usecases/FizzBuzz.spec.ts
+++ src/usecases/FizzBuzz.spec.ts
@@ -30,6 +30,34 @@ describe('verificaNumeroDivisivel', () => {
     const fizzbuzz = fizzBuzz.verificaNumeroDivisivel(numDivisivel);
     expect(fizzbuzz).toEqual(numDivisivel);
   });
+
+  test('retorna "Fizz" quando o numero contem o algarismo 3 e nao e divisivel por 3 ou 5', ()=>{
+    const numDivisivel = 23;
+    const fizzBuzz = new FizzBuzz()
+    const fizzbuzz = fizzBuzz.verificaNumeroDivisivel(numDivisivel);
+    expect(fizzbuzz).toEqual('Fizz');
+  });
+
+  test('retorna "Buzz" quando o numero contem o algarismo 5 e nao e divisivel por 3 ou 5', ()=>{
+    const numDivisivel = 58;
+    const fizzBuzz = new FizzBuzz()
+    const fizzbuzz = fizzBuzz.verificaNumeroDivisivel(numDivisivel);
+    expect(fizzbuzz).toEqual('Buzz');
+  });
+
+  test('retorna "BuzzFizz" quando o numero contem os algarismo 3 e 5 e nao e divisivel por 3 ou 5 e o 5 vem primeiro', ()=>{
+    const numDivisivel = 53;
+    const fizzBuzz = new FizzBuzz()
+    const fizzbuzz = fizzBuzz.verificaNumeroDivisivel(numDivisivel);
+    expect(fizzbuzz).toEqual('BuzzFizz');
+  });
+
+  test('retorna "FizzBuzz" quando o numero contem o algarismo 3 e 5 e nao e divisivel por 3 ou 5 e o 3 vem primeiro', ()=>{
+    const numDivisivel = 353;
+    const fizzBuzz = new FizzBuzz()
+    const fizzbuzz = fizzBuzz.verificaNumeroDivisivel(numDivisivel);
+    expect(fizzbuzz).toEqual('FizzBuzz');
+  });
 })  
 
 // Neste problema, você deverá exibir uma lista de 1 a 100, um em cada linha, com as seguintes exceções:
@@ -37,3 +65,9 @@ describe('verificaNumeroDivisivel', () => {
 // Números divisíveis por 3 deve aparecer como 'Fizz' ao invés do número;
 // Números divisíveis por 5 devem aparecer como 'Buzz' ao invés do número;
 // Números divisíveis por 3 e 5 devem aparecer como 'FizzBuzz' ao invés do número'.
+
+// Após resolver o problema do FizzBuzz acrescente a seguintes 
+// regra: - Números contendo o algarismo 3 devem aparecer como 'Fizz'. 
+// - Números contendo o algarismo 5 devem aparecer como 'Buzz'. 
+// - Números contendo ambos os algarismos devem aparecer como 'FizzBuzz', se o 3 vier antes do 5 
+// ou 'BuzzFizz' caso contrário. Obs.: Vale lembrar que as 3 regras do primeiro problema continuam valendo.
--- src/usecases/FizzBuzz.ts
+++ src/usecases/FizzBuzz.ts
@@ -9,9 +9,22 @@ class FizzBuzz {
         if (number % 5 === 0) {
             return 'Buzz'
         }
-
+        if(`${number}`.includes('3') && `${number}`.includes('5')) {
+          const positionThree = `${number}`.indexOf('3');
+          const positionFive = `${number}`.indexOf('5');
+          if(positionThree < positionFive) {
+            return 'FizzBuzz'
+          } else {
+            return 'BuzzFizz'
+          }
+        }
+        if (`${number}`.includes('3') && !`${number}`.includes('5')) {
+            return 'Fizz'
+        }
+        if (`${number}`.includes('5') && !`${number}`.includes('3')) {
+            return 'Buzz'
+        }
         return number;
-        
     }
 }
 
