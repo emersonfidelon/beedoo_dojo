51628635f7a139ef6d51d9e15563de15f02ed7be
Emerson Santana <emersonfidelon@gmail.com>
2021-05-26 15:52:25 -0300

    feat: include initial class

---
+++ src/usecases/GroupNumbers.spec.ts
@@ -0,0 +1,13 @@
+describe('GroupNumbers', () => {
+  test('', ()=>{
+    
+  });
+})  
+
+// Dado uma lista de números inteiros, agrupe a lista em um conjunto de intervalos
+
+// Exemplo:
+
+// Entrada: 100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150
+
+// Saída: [100-105], [110-111], [113-115], [150]
