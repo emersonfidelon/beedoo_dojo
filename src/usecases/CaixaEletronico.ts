96ce9aad6d4be49fd213105af443298851cbcfe5
Rafaela Ribeiro <rapha11br@gmail.com>
2021-05-30 12:20:53 -0300

    Caixa Eletronico - Rafaela Ribeiro

--- src/usecases/CaixaEletronico.ts
+++ src/usecases/CaixaEletronico.ts
@@ -1,24 +1,29 @@
-export function caixaEletronico(valor_saque:number) {
-
-    const notas_disponiveis = [100, 50, 20,10];
-
-    if(notas_disponiveis.includes(valor_saque)) {
-        return valor_saque;
-    }
+export function caixaEletronico(notas, valor_saque) {
     
+    if (valor_saque <= 0) { return 'Insira um número válido' }
+     
     if(valor_saque % 10 !==0){
-        return 'Ná há notas disponíveis para o valor informado.';
+        return 'Não há notas disponíveis para o valor informado.';
     }
+     for (var i = 0; i < notas.length; i++) {
+            if (valor_saque >= notas[i]) {
+                var nota = notas[i]
+            }
+    }    
+       var qtdeNotas = Math.floor(valor_saque / nota)
+        var resto= (valor_saque % nota);    
 
-    let notas = []
-    notas_disponiveis.map(nota => {
-        while (valor_saque < nota) {
-            
-        }
-    })
 
-    return 'Entregar 1 nota de R$100,00 e 1 nota de R$ 10,00.';
+        if ((resto != 0) && (resto > 1)) {
+            caixaEletronico(notas, resto);
+        }   
+
+    return `Entregar ${Math.floor(qtdeNotas)} ${(qtdeNotas) === 1 ? 'nota' : 'notas'} de R$ ${nota},00.`
 }
+const notas_disponiveis = [10, 20, 50, 100];
+
+
+
 
 
 
