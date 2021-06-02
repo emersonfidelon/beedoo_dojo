d25f6b2254ef429fc98a3905dcc37a39bfe626be
Emerson Santana <emersonfidelon@gmail.com>
2021-06-02 13:32:47 -0300

    Group Numbers

--- src/usecases/GroupNumbers.spec.ts
+++ src/usecases/GroupNumbers.spec.ts
@@ -1,6 +1,18 @@
+import { GroupNumbers } from "./GroupNumbers";
+
 describe('GroupNumbers', () => {
-  test('', ()=>{
-    
+  test('Deve retornar a propria lista caso esteja na sequencia correta', ()=>{
+    const sut = new GroupNumbers();
+    const list = [1,2,3,4,5];
+    const result = sut.handle(list)
+    expect(list).toEqual(result)
+  });
+
+  test('Deve retornar a duas listas caso nao esteja na sequencia correta', ()=>{
+    const sut = new GroupNumbers();
+    const list = [1, 2, 3, 4, 5, 7, 8, 9];
+    const result = sut.handle(list)
+    expect(result.length).toBe(2)
   });
 })  
 
@@ -9,5 +21,5 @@ describe('GroupNumbers', () => {
 // Exemplo:
 
 // Entrada: 100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150
-
 // Saída: [100-105], [110-111], [113-115], [150]
+
--- src/usecases/GroupNumbers.ts
+++ src/usecases/GroupNumbers.ts
@@ -1,5 +1,27 @@
 class GroupNumbers {
+  handle (list: Array<number>) {
+    const result = []    
+    let currentGroup = []
     
+    list.forEach((item, index) => {
+      let check = typeof list[index + 1] !== 'undefined' ? list[index + 1] - item : 0 // 1 - 2 
+      if(check == 1){
+        currentGroup.push(item) // [1,2]
+      }else{        
+        if(item == list[list.length-1]){
+          currentGroup.push(item)
+          result.push(currentGroup)    
+        }        
+        else {
+          result.push(currentGroup) // []
+          currentGroup = []
+          currentGroup.push(item) // [5]
+        }
+      }
+    })
+    console.log(result)
+    return result.length == 1 ? result[0] : result
+  }
 }
 
 export { GroupNumbers }
