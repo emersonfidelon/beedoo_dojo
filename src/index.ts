708c30b369dc71bf5f38c779e94412a8845055d5
Adriano <adrianomoreira2014@outlook.com>
2021-06-25 09:39:46 -0300

    alterada chamada ao método

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { BreakLine } from "./usecases/BreakLine";
 
-const breakLine = new BreakLine('Um pequeno jabuti xereta viu dez cegonhas felizes.', 10);
+const breakLine = new BreakLine({
+  frase: 'Um pequeno jabuti xereta viu dez cegonhas felizes.',
+  colunas: 0
+});
+
 console.log(breakLine.handle());
