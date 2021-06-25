5cae69368eefa5596afca25f49ba76da97e2923c
Igor Ulian <igorulian@outlook.com>
2021-06-25 20:43:36 -0300

    frases dos testes corrigidas

--- src/usecases/CaixaEletronico.spec.ts
+++ src/usecases/CaixaEletronico.spec.ts
@@ -5,7 +5,7 @@ describe('Teste Caixa Eletronico Dojo', () => {
     test('ao sacar 10 deve retornar 10', () => {
         const VALOR_SAQUE = 10;
         const sacado = caixaEletronico(VALOR_SAQUE);
-        expect(sacado).toEqual('Entregar 1 nota de R$10,00');
+        expect(sacado).toEqual('Entregar 1 nota de R$10,00.');
     });
 
     test('verifica se valor passado é multiplo de 10', () => {
@@ -17,19 +17,19 @@ describe('Teste Caixa Eletronico Dojo', () => {
     test('dado que valor passado 110 deve retornar 1 nota de 100 e uma de 10', () => {
         const VALOR_SAQUE = 110;
         const sacado = caixaEletronico(VALOR_SAQUE);
-        expect(sacado).toEqual('Entregar 1 nota de R$100,00 e 1 nota de R$ 10,00.')
+        expect(sacado).toEqual('Entregar 1 nota de R$100,00 e 1 nota de R$10,00.')
     })
 
     test('dado que valor passado 180 deve retornar 1 nota de 100, 1 de 50, 1 de 20 e 1 de 10', () => {
         const VALOR_SAQUE = 180;
         const sacado = caixaEletronico(VALOR_SAQUE);
-        expect(sacado).toEqual('Entregar 1 nota de R$100,00, 1 nota de R$ 50,00, 1 nota de R$ 20,00, 1 nota de R$ 10,00.')
+        expect(sacado).toEqual('Entregar 1 nota de R$100,00, 1 nota de R$50,00, 1 nota de R$20,00 e 1 nota de R$10,00.')
     })
 
     test('dado que valor passado 460 deve retornar 4 notas de 100, 1 de 50, 1 de 10', () => {
         const VALOR_SAQUE = 460;
         const sacado = caixaEletronico(VALOR_SAQUE);
-        expect(sacado).toEqual('Entregar 4 notas de R$100,00, 1 nota de R$ 50,00 e 1 nota de R$ 10,00')
+        expect(sacado).toEqual('Entregar 4 notas de R$100,00, 1 nota de R$50,00 e 1 nota de R$10,00.')
     })
 
 })
