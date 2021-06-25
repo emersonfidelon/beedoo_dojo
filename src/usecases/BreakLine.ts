03c6c2b800bb7bdad8735415214e05ff11d32ebe
Adriano <adrianomoreira2014@outlook.com>
2021-06-25 09:54:37 -0300

    alterado metodo handleworld

--- src/usecases/BreakLine.ts
+++ src/usecases/BreakLine.ts
@@ -1,53 +1,41 @@
-import { IBreakLine } from "../interfaces/IBreakLine";
+import IBreakLineService from "../interfaces/IBreakLineService";
 import { IBreakLineProps } from "../models/IBreakLineProps";
+import BreakLineService from "../services/BreakLineService";
+import Validations from '../validations';
 
-class BreakLine implements IBreakLine {
-    private frase: String;
-    private colunas: Number;
+class BreakLine {
+    private frase: string;
+    private colunas: number;
     private countQuebraLinha: number;
+    private breakLineService: IBreakLineService;
 
     constructor({ colunas, frase }: IBreakLineProps) {
+        this.breakLineService = new BreakLineService();
+
         this.frase = frase;
         this.colunas = colunas;
         this.countQuebraLinha = 0;
 
+
         this.validate();
     }
 
-    public handle(): string {
-        let resultado = '';
-        const palavras = this.frase.trim().split(' ');
-
-        let linha = '';
-        for (const palavra of palavras) {
-            if (linha.length + palavra.length < this.colunas) {
-                if (!linha)
-                    linha += palavra;
-                else
-                    linha += ' ' + palavra;
-            } else {
-                resultado += `${linha}\n`;
-                linha = palavra;
-            }
-        }
-
-        if (linha) {
-            resultado += linha;
-        }
+    public handleWord(): string {
+        const resultado = this.breakLineService.execute({
+            colunas: this.colunas,
+            frase: this.frase
+        });
 
         return resultado.trim();
     }
 
     private validate(): void {
-        if (this.frase.length === 0)
-            throw new Error('A frase deve conter ao menos um caractere')
+        const validation = new Validations();
 
-        if (this.colunas < 1)
-            throw new Error('A quantidade de coluna deve ser maior do que zero')
-
-        if (!Number.isInteger(this.colunas))
-            throw new Error('A quantidade de coluna deve ser um inteiro')
+        validation.hasMinLen(this.frase, 1, 'A frase deve conter ao menos um caractere');
+        validation.isLowerThan(this.colunas, 1, 'A quantidade de coluna deve ser maior do que zero');
+        validation.isInteger(this.colunas, 'A quantidade de coluna deve ser um inteiro');
     }
 }
 
-export { BreakLine }
+export default BreakLine
