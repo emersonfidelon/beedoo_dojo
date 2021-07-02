bdb62f5755f6ac36469bc095fe25c96d6d421941
Timóteo <timoteoapolinario@gmail.com>
2021-07-02 00:18:56 -0300

    abstraindo regras repetidas

--- src/usecases/JokenPoJuiz.ts
+++ src/usecases/JokenPoJuiz.ts
@@ -1,17 +1,36 @@
 export default class JokenPoJuiz {
     private readonly jogadasValidas = ['Pedra', 'Papel', 'Tesoura'];
-        
-    private readonly jogadasGanhadoras = {
-        'PedraTesoura':'Pedra ganhou',
-        'TesouraPedra':'Pedra ganhou',
-        'TesouraPapel':'Tesoura ganhou',
-        'PapelTesoura':'Tesoura ganhou',
-        'PapelPedra':'Papel ganhou',
-        'PedraPapel':'Papel ganhou',
-    }
 
     jogadaInvalida(jogador1, jogador2): boolean {
         return (!this.jogadasValidas.includes(jogador1) || !this.jogadasValidas.includes(jogador2))
+    }
+
+    private readonly ganhaPerde = {
+        'Pedra':'Tesoura',
+        'Tesoura':'Papel',
+        'Papel':'Pedra',
+    }
+
+    compara(ganha, perde, jogador1, jogador2): string {
+        let result: string = '';
+
+        if((`${jogador1}${jogador2}` == `${ganha}${perde}`) || (`${jogador1}${jogador2}` == `${perde}${ganha}`)){
+            result = `${ganha} ganhou`;
+        }
+
+        return result;
+    }
+
+    ganhador(jogador1, jogador2): string {
+        let result: string = '';
+
+        for (const ganha of Object.keys(this.ganhaPerde)){
+            let perde : string  = this.ganhaPerde[ganha]
+
+            if (result = this.compara(ganha, perde, jogador1, jogador2)) break;
+        };
+
+        return result;
     }
 
     verificar(jogador1, jogador2): string {
@@ -20,7 +39,7 @@ export default class JokenPoJuiz {
         if ( this.jogadaInvalida(jogador1, jogador2) ) {
             result = 'Jogada Inválida';
         } else if ( jogador1 != jogador2 ) {
-            result = this.jogadasGanhadoras[`${jogador1}${jogador2}`];
+            result = this.ganhador(jogador1, jogador2);
         }
 
         return result;
