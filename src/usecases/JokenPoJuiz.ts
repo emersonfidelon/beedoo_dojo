5c8c83b8072afe395e8af2fc89a81fef6d59af3e
Timóteo <timoteoapolinario@gmail.com>
2021-07-01 23:59:50 -0300

    Diminuindo para 5 a complexidade de verificar()

--- src/usecases/JokenPoJuiz.ts
+++ src/usecases/JokenPoJuiz.ts
@@ -10,16 +10,19 @@ export default class JokenPoJuiz {
         'PedraPapel':'Papel ganhou',
     }
 
-    verificar(jogador1, jogador2): String {
-        if (!this.jogadasValidas.includes(jogador1) || !this.jogadasValidas.includes(jogador2)) {
-            return 'Jogada Inválida';
+    jogadaInvalida(jogador1, jogador2): boolean {
+        return (!this.jogadasValidas.includes(jogador1) || !this.jogadasValidas.includes(jogador2))
+    }
+
+    verificar(jogador1, jogador2): string {
+        let result: string = 'Empate';
+
+        if ( this.jogadaInvalida(jogador1, jogador2) ) {
+            result = 'Jogada Inválida';
+        } else if ( jogador1 != jogador2 ) {
+            result = this.jogadasGanhadoras[`${jogador1}${jogador2}`];
         }
 
-        if (jogador1 == jogador2) {
-            return 'Empate';
-        }
-
-        return this.jogadasGanhadoras[`${jogador1}${jogador2}`]
-
+        return result;
     }
 }
