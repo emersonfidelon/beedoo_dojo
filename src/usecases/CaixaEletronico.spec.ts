bf642230cd6ccc8907940a2b304c530f12e602ac
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2021-07-28 18:43:32 -0300

    Criados mais 2 testes adicionais, criada a classe CaixaEletronico com a função sacar como uma promisse para poder utilizar o asyn/await

--- src/usecases/CaixaEletronico.spec.ts
+++ src/usecases/CaixaEletronico.spec.ts
@@ -1,23 +1,42 @@
-import {caixaEletronico} from './CaixaEletronico'
+'strict'
 
+import {CaixaEletronico} from './CaixaEletronico'
+
+let caixa_eletronico: CaixaEletronico;
 describe('Teste Caixa Eletronico Dojo', () => {
 
-    test('ao sacar 10 deve retornar 10', () => {
+    beforeEach(() => {
+        caixa_eletronico = new CaixaEletronico();
+      });
+
+    test('ao sacar 10 deve retornar 10', async () => {
         const VALOR_SAQUE = 10;
-        const sacado = caixaEletronico(VALOR_SAQUE);
+        const sacado = await caixa_eletronico.sacar(VALOR_SAQUE);
         expect(sacado).toEqual(10);
     });
 
-    test('verifica se valor passado é multiplo de 10', () => {
+    test('verifica se valor passado é multiplo de 10', async () => {
         const VALOR_SAQUE = 15;
-        const sacado = caixaEletronico(VALOR_SAQUE);
+        const sacado = await caixa_eletronico.sacar(VALOR_SAQUE);
         expect(sacado).toEqual('Ná há notas disponíveis para o valor informado.')
     })
 
-    test('dado que valor passado 110 deve retornar 1 nota de 100 e uma de 10', () => {
+    test('dado que valor passado 110 deve retornar 1 nota de 100 e uma de 10', async () => {
         const VALOR_SAQUE = 110;
-        const sacado = caixaEletronico(VALOR_SAQUE);
-        expect(sacado).toEqual('Entregar 1 nota de R$100,00 e 1 nota de R$ 10,00.')
+        const sacado = await caixa_eletronico.sacar(VALOR_SAQUE);
+        expect(sacado).toEqual('Entregar 1 nota de R$100,00 - 1 nota de R$10,00')
+    })
+
+    test('dado que valor passado 230 deve retornar 2 notas de 100 1 de 20 e uma de 10', async () => {
+        const VALOR_SAQUE = 230;
+        const sacado = await caixa_eletronico.sacar(VALOR_SAQUE);
+        expect(sacado).toEqual('Entregar 2 notas de R$100,00 - 1 nota de R$20,00 - 1 nota de R$10,00')
+    })
+
+    test('dado que valor passado 270 deve retornar 2 nota de 100, 1 nota de 50 e uma de 20', async () => {
+        const VALOR_SAQUE = 270;
+        const sacado = await caixa_eletronico.sacar(VALOR_SAQUE);
+        expect(sacado).toEqual('Entregar 2 notas de R$100,00 - 1 nota de R$50,00 - 1 nota de R$20,00')
     })
 
 })
--- src/usecases/CaixaEletronico.ts
+++ src/usecases/CaixaEletronico.ts
@@ -1,27 +1,44 @@
-export function caixaEletronico(valor_saque:number) {
-
-    const notas_disponiveis = [100, 50, 20,10];
-
-    if(notas_disponiveis.includes(valor_saque)) {
-        return valor_saque;
-    }
-    
-    if(valor_saque % 10 !==0){
-        return 'Ná há notas disponíveis para o valor informado.';
-    }
-
-    let notas = []
-    notas_disponiveis.map(nota => {
-        while (valor_saque < nota) {
+const notas_disponiveis = [100, 50, 20,10]; 
+class CaixaEletronico {
+    sacar(valor_saque:number){
+        return new Promise<number | string>((resolve, reject) => {
+            if(notas_disponiveis.includes(valor_saque)) {
+                resolve(valor_saque)
+            }
             
-        }
-    })
-
-    return 'Entregar 1 nota de R$100,00 e 1 nota de R$ 10,00.';
+            if(valor_saque % 10 !== 0){
+                resolve('Ná há notas disponíveis para o valor informado.')
+            }
+            let notas = []
+            notas_disponiveis.map(nota => {
+                while (valor_saque >= nota) {
+                    let nota_ja_existe = notas.findIndex(nota_para_sacar => {
+                        return nota_para_sacar.valor === nota;
+                    })
+                    if(nota_ja_existe >= 0){
+                        notas[nota_ja_existe] = {
+                            valor: nota,
+                            qtd: notas[nota_ja_existe].qtd + 1
+                        }
+                    }else{
+                        notas.push({valor: nota, qtd: 1});
+                    }
+                    valor_saque = valor_saque - nota
+                }
+            })           
+            let notas_para_disponibilizar = notas.map(nota => {
+                if(nota.qtd > 1){
+                    return `${nota.qtd} notas de R$${nota.valor},00`
+                }
+                return `1 nota de R$${nota.valor},00`
+            });
+            let msg_notas_para_entregar = `Entregar ${notas_para_disponibilizar.join(' - ')}`
+            resolve(msg_notas_para_entregar)
+        })
+    }
 }
 
-
-
+export { CaixaEletronico }
 
 // Desenvolva um programa que simule a entrega de notas quando um cliente efetuar um saque em um caixa
 // eletrônico. Os requisitos básicos são os seguintes:
