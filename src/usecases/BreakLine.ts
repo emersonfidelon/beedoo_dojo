cbb77e680bbfa5119a5832f3ab7f00b4bf308a6f
Nicasiomarques <nicasiomarques18@gmail.com>
2021-08-03 23:32:04 +0100

    refector to functional way

--- src/usecases/BreakLine.ts
+++ src/usecases/BreakLine.ts
@@ -1,34 +1,14 @@
-import {
-  wordIsToShort,
-  columnsShouldBeNumber,
-  columnsNumberLessThanZero,
-} from "./BreakLinesMessages"
+import { IBreakLineProps, IBreakLineValidator } from "./BreakLine.types"
+import { breakLineValidator } from "./BreakLinesValidator"
 
-export class BreakLine {
-  phase: string
-  columns: number
-  countBreakRow = 0
-
-  constructor(phase: string, columns: number) {
-    this.phase = phase
-    this.columns = columns
-    this.check()
-  }
-
-  run() {
-    return this.phase.split('').forEach(phase => {
+const makeBreakLine = (breakLineValidator: IBreakLineValidator) =>
+  ({ phase, columns }: IBreakLineProps) => {
+    breakLineValidator({ columns, phase })
+    return phase.split('').forEach(phase => {
       const phaseFinal = ''
-      if (phaseFinal.length > this.columns) {
-        phaseFinal.concat('\n')
-        this.countBreakRow++
-      }
+      if (phaseFinal.length > columns) phaseFinal.concat('\n')
       phaseFinal.concat(` ${phase}`)
     })
   }
 
-  check() {
-    if (!this.phase.length) throw new Error(wordIsToShort)
-    if (this.columns < 1) throw new Error(columnsNumberLessThanZero)
-    if (!Number.isInteger(this.columns)) throw new Error(columnsShouldBeNumber)
-  }
-}
+export const BreakLine = makeBreakLine(breakLineValidator);
