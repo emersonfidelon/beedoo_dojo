409f090ab506e6a82b1edb591b805c240c59ca57
Nicasiomarques <nicasiomarques18@gmail.com>
2021-08-03 23:33:29 +0100

    rename error message columnsShouldBeNumber to columnsShouldBeInteger

--- src/usecases/BreakLinesMessages.ts
+++ src/usecases/BreakLinesMessages.ts
@@ -1,3 +1,3 @@
 export const wordIsToShort = 'A frase deve conter ao menos um caractere';
 export const columnsNumberLessThanZero = 'A quantidade de colunas deve ser maior do que zero';
-export const columnsShouldBeNumber = 'A quantidade de coluna deve ser um inteiro';
+export const columnsShouldBeInteger = 'A quantidade de coluna deve ser um inteiro';
