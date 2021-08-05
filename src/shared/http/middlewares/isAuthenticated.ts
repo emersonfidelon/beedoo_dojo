be33e7a9356bf39324293274a282bc62748f9c3a
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-05 09:59:09 -0300

    global middleware to check if the user is authenticated created

---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,39 @@
+import jwt from "@config/auth/jwt";
+import AppError from "@shared/errors/AppError";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface ITokenPayLoad {
+  iat: number;
+  ext: number;
+  sub: string;
+}
+
+export default function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const headerAuth = req.headers.authorization;
+
+  if (!headerAuth) {
+    throw new AppError("Token is missing.", 401);
+  }
+
+  const [, token] = headerAuth.split(" ");
+
+  try {
+    const decodedToken = verify(token, jwt.secret);
+    const { sub } = decodedToken as ITokenPayLoad;
+
+    req.user = {
+      email: sub,
+    };
+
+    console.log(req.user)
+
+    return next();
+  } catch {
+    throw new AppError("Invalid token.", 401);
+  }
+}
