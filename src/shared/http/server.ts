b49ab90cefcbeea08385b4974c1f4545f8267c08
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-04 22:04:57 -0300

    server config file created

---
+++ src/shared/http/server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+
+const listenPort = process.env.PORT || 3000;
+
+app.listen(listenPort, () => {
+    console.log(`[beedoo-dojo] Server is running on port ${listenPort}!`)
+});
