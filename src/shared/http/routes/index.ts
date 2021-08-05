65ed4dcb2e9ae7b5df0ede62a77f48496fa6d7e7
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-05 09:34:05 -0300

    sessions routes imported to main routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import usersRouter from "@UsersRoutes";
+import sessionsRouter from "@SessionsRoutes";
 import { Router } from "express";
 
 const router = Router();
@@ -7,5 +8,6 @@ router.get("/", (req, res) => {
     res.send("Seja bem-vindo ao Banco Beedoo!");
 });
 router.use("/users", usersRouter);
+router.use("/sessions", sessionsRouter);
 
 export default router;
