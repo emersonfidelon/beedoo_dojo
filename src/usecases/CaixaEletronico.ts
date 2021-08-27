dd4e73eaecc33d0923f7ca02823442efe63d3a9d
Raul743 <raulinacio233@gmail.com>
2021-08-27 11:00:38 +0100

    feature : termino:caixa electronico

--- src/usecases/CaixaEletronico.ts
+++ src/usecases/CaixaEletronico.ts
@@ -1,27 +1,44 @@
-export function caixaEletronico(valor_saque:number) {
+export function caixaEletronico(valor_saque: number) {
+  const notas_disponiveis = [100, 50, 20, 10];
 
-    const notas_disponiveis = [100, 50, 20,10];
+  var cont: boolean = false;
+  var string_concat: string;
 
-    if(notas_disponiveis.includes(valor_saque)) {
-        return valor_saque;
+  if (notas_disponiveis[notas_disponiveis.length - 1] > valor_saque)
+    return "Não há notas disponíveis para o valor informado.";
+
+  if (notas_disponiveis.includes(valor_saque)) {
+    return valor_saque;
+  }
+
+  while (valor_saque >= notas_disponiveis[notas_disponiveis.length - 1]) {
+    let note: number = 0;
+    for (let i = 0; i < notas_disponiveis.length; i++) {
+      if (valor_saque >= notas_disponiveis[i]) {
+        note = notas_disponiveis[i];
+        break;
+      }
     }
-    
-    if(valor_saque % 10 !==0){
-        return 'Ná há notas disponíveis para o valor informado.';
+
+    const div = Math.trunc(valor_saque / note);
+
+    // console.log("Vais entregar %s notas de R$ %s", div, note);
+
+    if (!cont) {
+      string_concat = `Entregar ${div} nota(s) de R$${note},00`;
+    } else {
+      string_concat = `${string_concat} e ${div} nota(s) de R$ ${note},00`;
     }
+    cont = true;
+    valor_saque -= note * div;
+  }
 
-    let notas = []
-    notas_disponiveis.map(nota => {
-        while (valor_saque < nota) {
-            
-        }
-    })
-
-    return 'Entregar 1 nota de R$100,00 e 1 nota de R$ 10,00.';
+  if (valor_saque > 0) {
+    return `${string_concat}, Ainda restaram R$${valor_saque},00 na sua conta`;
+  }
+  return string_concat + ".";
+  // return "Entregar 1 nota de R$100,00 e 1 nota de R$ 10,00.";
 }
-
-
-
 
 // Desenvolva um programa que simule a entrega de notas quando um cliente efetuar um saque em um caixa
 // eletrônico. Os requisitos básicos são os seguintes:
