bcad8b08ac2162041eddd41c5427c5b05c77ff30
Ramon Nunes <ramonkjl@gmail.com>
2021-08-27 23:06:28 -0300

    Criado classe para verificar se qual das duas jogadas são inválidas ou se ambas são inválidas.

---
+++ src/errors/jogada-invalida.ts
@@ -0,0 +1,14 @@
+export class JogadaInvalida {
+
+  static lacarErro(jogadasValidas: Array<JogadaValida>, jogada1: JogadaValida, jogada2: JogadaValida) {
+    if (!jogadasValidas.includes(jogada1) && !jogadasValidas.includes(jogada2))
+      throw new Error(`${jogada1} e ${jogada2} não são jogadas válidas`)
+
+    if (!jogadasValidas.includes(jogada1))
+      throw new Error(`${jogada1} não é uma jogada válida`)
+
+    if (!jogadasValidas.includes(jogada2))
+      throw new Error(`${jogada2} não é uma jogada válida`)
+  }
+
+}
