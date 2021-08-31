6e77a7864f02250bfc2131c596411d3c4b2b667a
Isolda <isoldamunizb@gmail.com>
2021-08-31 13:06:53 +0200

    translate to english

--- src/usecases/BreakLine.spec.ts
+++ src/usecases/BreakLine.spec.ts
@@ -1,39 +1,42 @@
 import BreakLine from './BreakLine';
 
-describe('Break Line', () => {
-  test('Verifica se a entrada esta correta', () => {
+describe('# Break Line #', () => {
+  test('check if the entry is correct', () => {
     const text = 'teste um dois tres';
     const columns = 20;
     const breakLine = new BreakLine(text, columns);
     expect(breakLine).toBeInstanceOf(BreakLine);
   });
 
-  test('Retorna erro com uma frase vazia', () => {
+  test('return error when text is empty', () => {
     const text = '';
     const columns = 20;
-    expect(() => new BreakLine(text, columns)).toThrow(new Error('A frase deve conter ao menos um caractere'));
+    expect(() => new BreakLine(text, columns))
+      .toThrow(new Error('A frase deve conter ao menos um caractere'));
   });
 
-  test('Retorna erro quando colunas for menor que 1', () => {
+  test('return error when columns is less than 1', () => {
     const text = 'teste um dois tres';
     const columns = 0;
-    expect(() => new BreakLine(text, columns)).toThrow(new Error('A quantidade de coluna deve ser maior do que zero'));
+    expect(() => new BreakLine(text, columns))
+      .toThrow(new Error('A quantidade de coluna deve ser maior do que zero'));
   });
 
-  test('Retorna erro quanto o numero de colunas nao for um inteiro', () => {
+  test('return error when columns is not an integer', () => {
     const text = 'teste um dois tres';
     const columns = 12.2;
-    expect(() => new BreakLine(text, columns)).toThrow(new Error('A quantidade de coluna deve ser um inteiro'));
+    expect(() => new BreakLine(text, columns))
+      .toThrow(new Error('A quantidade de coluna deve ser um inteiro'));
   });
 
-  test('Retorna texto formatado quando a frase contem menos colunas que o informado', () => {
+  test('return formatted text when text has fewer columns than entered', () => {
     const text = 'um cinco';
     const columns = 10;
     const breakLine = new BreakLine(text, columns);
     expect(breakLine.finalText).toBe('um cinco ');
   });
 
-  test('Retorna texto formatado quando a frase contem mais colunas que o informado', () => {
+  test('return formatted text when text has more columns than entered', () => {
     const text = 'Um pequeno jabuti xereta viu dez cegonhas felizes.';
     const columns = 20;
     const breakLine = new BreakLine(text, columns);
