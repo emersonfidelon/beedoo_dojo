4dc4d589bfcfab1558296569638b77ee99294831
Isolda <isoldamunizb@gmail.com>
2021-08-31 10:17:57 +0200

    fix prettier. remove unused variables and test coverage

--- src/usecases/BreakLine.spec.ts
+++ src/usecases/BreakLine.spec.ts
@@ -1,44 +1,50 @@
 import { BreakLine } from "./BreakLine";
 
-describe('Break Line', () => {
-
-  test('Verifica se a entrada esta correta', () => {
-    const text = 'teste um dois tres';
+describe("Break Line", () => {
+  test("Verifica se a entrada esta correta", () => {
+    const text = "teste um dois tres";
     const columns = 20;
-    const breakLine = new BreakLine(text, columns); 
+    const breakLine = new BreakLine(text, columns);
     expect(breakLine).toBeInstanceOf(BreakLine);
   });
 
-  test('Retorna erro com uma frase vazia', () => {
-    const text = '';
+  test("Retorna erro com uma frase vazia", () => {
+    const text = "";
     const columns = 20;
-    expect(() => {new BreakLine(text, columns)}).toThrow(new Error('A frase deve conter ao menos um caractere'));
+    expect(() => {
+      new BreakLine(text, columns);
+    }).toThrow(new Error("A frase deve conter ao menos um caractere"));
   });
 
-  test('Retorna erro quando colunas for menor que 1', () => {
-    const text = 'teste um dois tres';
+  test("Retorna erro quando colunas for menor que 1", () => {
+    const text = "teste um dois tres";
     const columns = 0;
-    expect(() => {new BreakLine(text, columns)}).toThrow(new Error('A quantidade de coluna deve ser maior do que zero'));
+    expect(() => {
+      new BreakLine(text, columns);
+    }).toThrow(new Error("A quantidade de coluna deve ser maior do que zero"));
   });
 
-  test('Retorna erro quanto o numero de colunas nao for um inteiro', () => {
-    const text = 'teste um dois tres';
+  test("Retorna erro quanto o numero de colunas nao for um inteiro", () => {
+    const text = "teste um dois tres";
     const columns = 12.2;
-    expect(() => {new BreakLine(text, columns)}).toThrow(new Error('A quantidade de coluna deve ser um inteiro'));
+    expect(() => {
+      new BreakLine(text, columns);
+    }).toThrow(new Error("A quantidade de coluna deve ser um inteiro"));
   });
 
-  test('Retorna texto formatado quando a frase contem menos colunas que o informado', () => {
-    const text = 'um cinco';
+  test("Retorna texto formatado quando a frase contem menos colunas que o informado", () => {
+    const text = "um cinco";
     const columns = 10;
     const breakLine = new BreakLine(text, columns);
-    expect(breakLine.finalText).toBe('um cinco ');
+    expect(breakLine.finalText).toBe("um cinco ");
   });
 
-  test('Retorna texto formatado quando a frase contem mais colunas que o informado', () => {
-    const text = 'Um pequeno jabuti xereta viu dez cegonhas felizes.';
+  test("Retorna texto formatado quando a frase contem mais colunas que o informado", () => {
+    const text = "Um pequeno jabuti xereta viu dez cegonhas felizes.";
     const columns = 20;
     const breakLine = new BreakLine(text, columns);
-    expect(breakLine.finalText).toBe('Um pequeno jabuti \nxereta viu dez \ncegonhas felizes. ');
+    expect(breakLine.finalText).toBe(
+      "Um pequeno jabuti \nxereta viu dez \ncegonhas felizes. "
+    );
   });
-  
 });
--- src/usecases/BreakLine.ts
+++ src/usecases/BreakLine.ts
@@ -1,48 +1,46 @@
 class BreakLine {
   text: string;
   columns: number;
-  countBreakLine: number = 0;
   finalText: string;
 
-  constructor(text:string, columns:number){
+  constructor(text: string, columns: number) {
     this.text = text;
     this.columns = columns;
-
-      this.validate();
+    this.validate();
   }
 
-handle(){
-    this.finalText = '';
-    let line = '';
-    for (const word of this.text.split(' ')) {
+  handle() {
+    this.finalText = "";
+    let line = "";
+    for (const word of this.text.split(" ")) {
       const newLine = `${line} ${word}`;
       if (newLine.length <= this.columns) {
         line += `${word} `;
       } else {
-        line += '\n';
+        line += "\n";
         this.finalText += line;
         line = `${word} `;
       }
     }
     this.finalText += line;
     return this.finalText;
+  }
+
+  validate() {
+    if (this.text.length === 0) {
+      throw new Error("A frase deve conter ao menos um caractere");
     }
 
-    validate() {
-        if (this.text.length === 0) {
-            throw new Error('A frase deve conter ao menos um caractere')
-        }
-
-        if (this.columns < 1) {
-            throw new Error('A quantidade de coluna deve ser maior do que zero')
-        }
-
-        if (!Number.isInteger(this.columns)) {
-            throw new Error('A quantidade de coluna deve ser um inteiro')
-        } else {
-            this.handle();
-        }
+    if (this.columns < 1) {
+      throw new Error("A quantidade de coluna deve ser maior do que zero");
     }
+
+    if (!Number.isInteger(this.columns)) {
+      throw new Error("A quantidade de coluna deve ser um inteiro");
+    } else {
+      this.handle();
+    }
+  }
 }
 
-export { BreakLine }
+export { BreakLine };
