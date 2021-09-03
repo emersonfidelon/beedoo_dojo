3c95a1ddaaae127a7e00289b6e79294f79a37f8b
Johnson Sudré <johnsonsudre@gmail.com>
2021-09-03 10:38:09 -0300

    Insere uma segunda opcao ao metodo handle da classe GroupNumbers, denominado handle2, criada com [reduce] no lugar do [for]

--- src/usecases/GroupNumbers.spec.ts
+++ src/usecases/GroupNumbers.spec.ts
@@ -39,3 +39,34 @@ describe('░▒▓█ Teste da classe GroupNumbers | metodo handle com for:', (
     expect(result.length).toEqual(3)
   })
 })
+
+describe('░▒▓█ Teste da classe GroupNumbers | metodo handle2 com reduce:', () => {
+  test('Deve retornar a propria lista com o elemento unico = 0.', () => {
+    const list = [0]
+    const result = sut.handle2(list)
+    expect(result).toEqual(list)
+  })
+  test('Deve retornar a propria lista com um elemento somente.', () => {
+    const list = [7]
+    const result = sut.handle2(list)
+    expect(result).toEqual(list)
+  })
+
+  test('Deve retornar a propria lista caso esteja na sequencia correta.', () => {
+    const list = [1, 2, 3, 4, 5]
+    const result = sut.handle2(list)
+    expect(result).toEqual(list)
+  })
+
+  test('Deve retornar duas listas caso nao esteja na sequencia correta.', () => {
+    const list = [1, 2, 3, 4, 5, 7, 8, 9]
+    const result = sut.handle2(list)
+    expect(result.length).toBe(2)
+  })
+
+  test('Deve retornar tres listas caso nao esteja na sequencia correta.', () => {
+    const list = [1, 2, 3, 1, 1, 2]
+    const result = sut.handle2(list)
+    expect(result.length).toEqual(3)
+  })
+})
--- src/usecases/GroupNumbers.ts
+++ src/usecases/GroupNumbers.ts
@@ -17,6 +17,23 @@ class GroupNumbers {
     // console.log(result)
     if (result.length > 1) { return result } else { return result[0] }
   }
+
+  handle2(list: number[]) {
+    let currentGroup: number[] = []
+    const result: number[][] = list.reduce((ac: number[][], val: number, i: number, list: number[]) => {
+      const nextVal = list[i + 1]
+      if (nextVal && nextVal - val === 1) {
+        currentGroup.push(val)
+      } else {
+        currentGroup.push(val)
+        ac.push(currentGroup)
+        currentGroup = []
+      }
+      return ac
+    }, [])
+    // console.log(result)
+    if (result.length > 1) { return result } else { return result[0] } // ou list
+  }
 }
 
 export { GroupNumbers }
