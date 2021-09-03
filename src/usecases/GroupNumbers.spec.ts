2b9b74487cc9839c5d0494fe88c6b305916f1ef1
Johnson Sudré <johnsonsudre@gmail.com>
2021-09-03 10:20:05 -0300

    fix eslint config e alteração/correção do teste (.spec) do metodo handle, da classe GroupNumbers

--- src/usecases/GroupNumbers.spec.ts
+++ src/usecases/GroupNumbers.spec.ts
@@ -1,20 +1,3 @@
-import { GroupNumbers } from "./GroupNumbers";
-
-describe('GroupNumbers', () => {
-  test('Deve retornar a propria lista caso esteja na sequencia correta', ()=>{
-    const sut = new GroupNumbers();
-    const list = [1,2,3,4,5];
-    const result = sut.handle(list)
-    expect(list).toEqual(result)
-  });
-
-  test('Deve retornar a duas listas caso nao esteja na sequencia correta', ()=>{
-    const sut = new GroupNumbers();
-    const list = [1, 2, 3, 4, 5, 7, 8, 9];
-    const result = sut.handle(list)
-    expect(result.length).toBe(2)
-  });
-})  
 
 // Dado uma lista de números inteiros, agrupe a lista em um conjunto de intervalos
 
@@ -23,3 +6,36 @@ describe('GroupNumbers', () => {
 // Entrada: 100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150
 // Saída: [100-105], [110-111], [113-115], [150]
 
+import { GroupNumbers } from './GroupNumbers'
+
+const sut = new GroupNumbers()
+
+describe('░▒▓█ Teste da classe GroupNumbers | metodo handle com for:', () => {
+  test('Deve retornar a propria lista com o elemento unico = 0.', () => {
+    const list = [0]
+    const result = sut.handle(list)
+    expect(result).toEqual(list)
+  })
+  test('Deve retornar a propria lista com um elemento somente.', () => {
+    const list = [7]
+    const result = sut.handle(list)
+    expect(result).toEqual(list)
+  })
+  test('Deve retornar a propria lista caso esteja na sequencia correta.', () => {
+    const list = [1, 2, 3, 4, 5]
+    const result = sut.handle(list)
+    expect(result).toEqual(list)
+  })
+
+  test('Deve retornar duas listas caso nao esteja na sequencia correta.', () => {
+    const list = [1, 2, 3, 4, 5, 7, 8, 9]
+    const result = sut.handle(list)
+    expect(result.length).toBe(2)
+  })
+
+  test('Deve retornar tres listas caso nao esteja na sequencia correta.', () => {
+    const list = [1, 2, 3, 1, 1, 2]
+    const result = sut.handle(list)
+    expect(result.length).toEqual(3)
+  })
+})
